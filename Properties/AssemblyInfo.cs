a6e42e0c3e1c4696374dfcbe1839318443010ca7
Arkhorse <theillusivemman@gmail.com>
2023-05-18 22:06:33 -0300

    2.0.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,10 +1,5 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-using MelonLoader;
+﻿using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("BedrollTweaker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,25 +9,9 @@ using MelonLoader;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1f13dece-605d-4e9f-a1ca-7873f55955df")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
-[assembly: MelonInfo(typeof(BedrollTweaker.Implementation), "BedrollTweaker", "2.0.0", "GruffCassquatch, Phaedrus")]
+[assembly: AssemblyVersion("2.0.1")]
+[assembly: AssemblyFileVersion("2.0.1")]
+[assembly: MelonInfo(typeof(BedrollTweaker.Implementation), "BedrollTweaker", "2.0.1", "GruffCassquatch, Phaedrus, The Illusion")]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
