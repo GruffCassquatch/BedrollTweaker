9507c204420308077a5041dc5b4aa27f7f9e1835
Arkhorse <theillusivemman@gmail.com>
2023-05-29 22:55:41 -0300

    Create Utilities.cs

---
+++ src/Utilities.cs
@@ -0,0 +1,20 @@
+﻿namespace BedrollTweaker
+{
+    internal class Utilities
+    {
+        internal static List<float> lBedRolls = new();
+        internal static float BedRollBonus = 0f;
+        public static string? NormalizeName(string name)
+        {
+            if (name == null)
+            {
+                return null;
+            }
+            else
+            {
+                return name.Replace("(Clone)", "").Trim();
+            }
+        }
+        public static GearItem GetGearItemPrefab(string name) => GearItem.LoadGearItemPrefab(name).GetComponent<GearItem>();
+    }
+}
